5f9a632f80b59c012788f60024bacdc9f902bdcf
freefreex3 <32857184+freefreex3@users.noreply.github.com>
2017-11-21 10:29:35 +0800

    Delete 172932FQ1.py

--- 172932FQ1.py
+++
@@ -1,8 +0,0 @@
-# Author : Tey Tong Yuan Justin
-# Admin No :172932F
-records =int(input("No of expense records you want to enter:"))
-sum = 0
-for n in range(1,records+1):
-    expense = float(input("Please enter the expenses amount:"))
-    sum=sum + expense
-print("Total expenses you have entered is $ %.2f"%(sum) )
