ad042b8e14dc0339d2db5f010408b4a920eaea71
afiqali <33055409+afiqali@users.noreply.github.com>
2017-12-08 10:09:33 +0800

    remove additional codes that is not needed

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ app = Flask(__name__)
 def Home():
     return render_template('home.html')
 
-@app.route('/')
-def Home():
-    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
