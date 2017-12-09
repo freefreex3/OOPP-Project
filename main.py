ee6f1653bb43d0d35e7aa2fbc52ebc302df8d286
LauYuKang <32857229+LauYuKang@users.noreply.github.com>
2017-12-09 14:37:22 +0800

    Add files via upload
    updated on 9/12

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ app = Flask(__name__)
 def Home():
     return render_template('home.html')
 
+@app.route('/home.html/')
+def home():
+    return render_template('home.html')
+
+@app.route('/Repair.html/')
+def Repair():
+    return render_template('Repair.html')
+
+@app.route('/Storage.html/')
+def Storage():
+    return render_template('Storage.html')
+
+@app.route('/Profile.html/')
+def Profile():
+    return render_template('Profile.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
+
