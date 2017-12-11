10edab9a98bb16edb4072d90d978d1d7ac9fc7d2
LauYuKang <32857229+LauYuKang@users.noreply.github.com>
2017-12-11 21:42:22 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from wtforms import Form, StringField, TextAreaField, RadioField, SelectField, PasswordField, validators
 app = Flask(__name__)
 
-@app.route('/')
-def Home():
-    return render_template('home.html')
+class LoginForm(Form):
+    username = StringField('Username', [validators.Length(min=1, max=150), validators.DataRequired()])
+    password = PasswordField('Password')
 
-@app.route('/home.html/')
+@app.route('/', methods=['GET', 'POST'])
+def log_in():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        if(form.username.data == "admin" and form.password.data == "password"):
+            return render_template('home.html', form=form)
+
+    return render_template('test.html', form=form)
+
+@app.route('/home/')
 def home():
     return render_template('home.html')
 
-@app.route('/Repair.html/')
+@app.route('/Repair/')
 def Repair():
     return render_template('Repair.html')
 
-@app.route('/Storage.html/')
+@app.route('/Storage/')
 def Storage():
     return render_template('Storage.html')
 
-@app.route('/Profile.html/')
+@app.route('/Profile/')
 def Profile():
     return render_template('Profile.html')
 
